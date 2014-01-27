151453e1f4d765b12ee7dcd75f9cb21aa1d8ea2f
Brian Marshall <bmarsd@gmail.com>
2014-01-27 00:32:31 -0800

    Remove blank line at top of license comment blocks
    Just an aesthetic choice.

--- src/image.h
+++ src/image.h
@@ -1,5 +1,4 @@
-/*
- * Copyright (C) 2014 Brian Marshall
+/* Copyright (C) 2014 Brian Marshall
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

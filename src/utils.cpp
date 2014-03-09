c50eeed5f4a3738cdbea70fbd3ecfdebab9e8d7b
Brian Marshall <bmarsd@gmail.com>
2014-03-08 17:11:03 -0800

    Multiple fixes to scale_to_fit()
    * Check that the image can fit with the scrollbar before returning 1.0.
    * Store width and height internally as doubles instead of ints to
      increase precision.
    * Check if resizing to fit with the scrollbar causes the height to
      shrink to less than 'dest_height'.
    
    These should fix some issues with zoom-to-fit making images too small or
    too big.

--- src/utils.cpp
+++ src/utils.cpp
@@ -27,25 +27,25 @@
 // both width and height are constrained.
 double scale_to_fit(int dest_width, int dest_height, int src_width,
     int src_height, bool expand, int scrollbar_width) {
-  int w, h;
   if (!expand && src_width <= dest_width && (src_height <= dest_height ||
-        scrollbar_width)) {
-    w = src_width;
-    h = src_height;
-  } else {
-    w = dest_width;
-    h = w * src_height / src_width;
-    if (h > dest_height) {
-      if (scrollbar_width) {
-        w = dest_width - scrollbar_width;
-        h = w * src_height / src_width;
-      } else {
-        h = dest_height;
-        w = src_width * h / src_height;
-      }
+        (scrollbar_width && src_width <= dest_width - scrollbar_width)))
+    return 1.0;
+
+  double w = dest_width;
+  double h = w * src_height / src_width;
+  if (std::round(h) > dest_height) {
+    if (scrollbar_width) {
+      w = dest_width - scrollbar_width;
+      h = w * src_height / src_width;
+    }
+    // Sometimes the image is no longer tall enough to cause a vertical
+    // scrollbar after subtracting the scrollbar width above.
+    if (!scrollbar_width || std::round(h) <= dest_height) {
+      h = dest_height;
+      w = src_width * h / src_height;
     }
   }
-  return (double(w) / src_width + double(h) / src_height) / 2;
+  return (w / src_width + h / src_height) / 2;
 }
 
 std::string to_percentage(double decimal) {
